e6715fd0ee444a7f3e9977b87c572264b94f0f13
Abel Rodríguez Pastoriza <abel.lisensiado@gmail.com>
2022-12-10 17:02:13 +0100

    build: Completado del esqueleto de la base de datos

--- bd_prj_ud3_cosmore.sql
+++ bd_prj_ud3_cosmore.sql
@@ -5,7 +5,7 @@ USE COSMERE;
 
 CREATE table Personajes(
 	id int not null primary key,
-	nombre varchar(50) not null,
+	nombre_personaje varchar(50) not null,
     fecha_nac varchar(5) null,
     especie varchar(20) not null,
     planeta_origen varchar(25) not null
@@ -13,27 +13,53 @@ CREATE table Personajes(
 
 CREATE table PLANETAS(
 	id int not null primary key,
-	nombre varchar(50) not null,
+	nombre_planeta varchar(50) not null,
     sistema varchar(40) not null,
-    squirla varchar(30) null,
+    esquirla varchar(30) null,
     saga int not null
 )engine = InnoDB;
 
-CREATE table LIBROS(
+CREATE table Libros(
 	id int not null primary key,
-	nombre varchar(50) not null,
+	nombre_libro varchar(50) not null,
     planeta varchar(30) null,
-    sistema varchar(40) not null,
     saga int not null
 )engine = InnoDB;
 
-CREATE table SAGAS;
+CREATE table Sagas(
+	id int not null primary key,
+	nombre_saga varchar(50) not null,
+    longitud varchar(2) not null
+    
+)engine = InnoDB;
 
-CREATE table ESPECIES;
+CREATE table Especies(
+	id_especie int not null primary key,
+	nombre_especie varchar(50) not null,
+    planeta_origen varchar(30) not null,
+    fisiologia varchar(50) not null
+)engine = InnoDB;
 
-CREATE table SISTEMAS_PLANETARIOS;
+CREATE table Sistemas_Planetarios(
+	id_sistema int not null primary key,
+	nombre_sistema varchar(50) not null,
+    numero_planetas varchar(2) not null
+)engine = InnoDB;
 
-create table magias;
+create table Magias(
+	id_magia int not null primary key,
+	nombre_magia varchar(30) not null,
+    esquirla varchar(30)
+)engine = InnoDB;
 
-create table personajes_sagas;
-create table personajes_magias;
+create table personajes_sagas(
+	id int not null primary key,
+    nombre_personaje varchar(50) not null,
+    numero_sagas varchar(1) not null 
+    
+)engine = InnoDB;
+create table personajes_magias(
+	id int not null primary key,
+    nombre_personaje varchar(50) not null,
+    nombre_magia varchar(30) not null
+)engine = InnoDB;
